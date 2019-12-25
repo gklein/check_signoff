65420799a750a187768fb5256f87120d30cd0df2
Gil Klein <gklein@users.noreply.github.com>
2019-12-25 16:59:12 +0200

    Reformat using black

--- check_signoff.py
+++ check_signoff.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import os
+
 import sys
 
 
@@ -10,14 +10,15 @@ def main():
     retval = 0
     commit_msg_path = sys.argv[1]
     with open(commit_msg_path) as f:
-        found = 'Signed-off-by: ' in f.read()
+        found = "Signed-off-by: " in f.read()
         if not found:
             retval = 1
-            print("The current git commit message is missing a 'Signed-off-by:' section")
+            print("The current git commit message is missing", end="")
+            print(" a 'Signed-off-by: section'")
             print("Please commit with 'git commit --signoff'")
 
     return retval
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(main())
